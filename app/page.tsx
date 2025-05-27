fde5687c873e225aad8563ea89030b1c4da53f45
radiamdev <166991055+radiamdev@users.noreply.github.com>
2025-05-27 08:39:36 +0300

    initialize project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header/>
+      <h1>Home</h1>
+      <Footer/>
+    </main>
+  );
+}
